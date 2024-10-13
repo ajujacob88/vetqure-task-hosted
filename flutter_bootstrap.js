9000bfc2301e3f74f6b2df8b005aaaa83678db02
ajujacob <ajujacob88@gmail.com>
2024-10-14 01:59:32 +0530

    Update build files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4064929460"
+    serviceWorkerVersion: "117721374"
   }
 });
